12226e66749e3d1cc18437d8bed02f7b337abefb
Gevorg0302 <gevorv@inbox.ru>
2017-09-23 20:47:13 +0400

    adding js

---
+++ homework3.js
@@ -0,0 +1,27 @@
+<script>
+const sum = function(first, second, third){ 
+	const result = (first*second/third);
+	return result;
+};
+const nothing = function(num1, num2){
+	const result1 = num1*num2;
+	return console.log;
+};
+const something = function(){
+	const a = "Hello World";
+	return a;
+};
+const fullName = function(name, surname){
+	const result2 = name + " " + surname");
+	return result2;
+};
+
+const cond = function(x,y){
+	if(x===y)
+		return "0";
+	if(x>y)
+		return "1";
+	if(x<y)
+		return "-1";
+};
+</script>
